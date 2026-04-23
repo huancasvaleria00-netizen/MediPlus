e3491325feeb1f156721bc679b39702c2a601282
huancasvaleria00-netizen <huancasvaleria00@gmail.com>
2026-04-22 19:46:24 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,1 +1,18 @@
+const mysql = require("mysql2");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mediplus_db"
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+module.exports = connection;
