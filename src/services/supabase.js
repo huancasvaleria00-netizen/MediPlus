ffa762e7012e104f506965be0dea7bc839617a96
huancasvaleria00-netizen <huancasvaleria00@gmail.com>
2026-04-22 20:48:10 -0500

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "TU_URL_AQUI";
-const supabaseKey = "TU_KEY_AQUI";
+const supabaseUrl = "http://localhost:3000";
+const supabaseKey = "sb_publishable_ZLBZw5RWx7E3gtB9xPSm2w_v08IDSj2";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
