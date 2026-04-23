8802733e11dfff5bbad729049cefc44c9fb62493
huancasvaleria00-netizen <huancasvaleria00@gmail.com>
2026-04-22 19:30:46 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Middleware personalizado
+const logger = require("./middlewares/logger");
+app.use(logger);
+
+// Rutas
+const medicamentosRoutes = require("./routes/medicamentos.routes");
+app.use("/api/medicamentos", medicamentosRoutes);
+
+// Ruta base
+app.get("/", (req, res) => {
+  res.send("API MediPlus funcionando 🚀");
+});
+
+// Servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
