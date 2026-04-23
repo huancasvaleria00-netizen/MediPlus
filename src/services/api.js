7076e292c3864d6011a39f4e771fe5fffae510c7
huancasvaleria00-netizen <huancasvaleria00@gmail.com>
2026-04-22 20:09:24 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,25 @@
+const API_URL = "http://localhost:3000/api/medicamentos";
 
+export const getMedicamentos = async () => {
+  try {
+    const res = await fetch(API_URL);
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching medicines:", error);
+    return [];
+  }
+};
+
+export const createMedicamento = async (data) => {
+  try {
+    await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  } catch (error) {
+    console.error("Error creating medicine:", error);
+  }
+};
