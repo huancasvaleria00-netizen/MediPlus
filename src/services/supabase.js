fcbaa2da7656a0b942f4e61a58f1e4783108dcba
huancasvaleria00-netizen <huancasvaleria00@gmail.com>
2026-04-22 20:51:03 -0500

    Delete src/services/supabase.js

--- src/services/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "http://localhost:3000";
-const supabaseKey = "sb_publishable_ZLBZw5RWx7E3gtB9xPSm2w_v08IDSj2";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
